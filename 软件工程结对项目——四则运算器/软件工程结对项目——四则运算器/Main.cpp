d56a38fba770270c1b8cab2ef1d4d839ae77d0c2
feimo49 <44833683+feimo49@users.noreply.github.com>
2019-01-19 11:16:25 +0800

    Add files via upload

--- 软件工程结对项目——四则运算器/软件工程结对项目——四则运算器/Main.cpp
+++ 软件工程结对项目——四则运算器/软件工程结对项目——四则运算器/Main.cpp
@@ -11,11 +11,17 @@ using namespace std;
 
 int main(int argc, char * argv[])
 {
+	if (argc < 3)
+	{
+		printf("Please input TWO parameters!\n");
+		system("pause");
+		return 0;
+	}
 	if (!strcmp(argv[1], "-i"))
 	{
 		srand((unsigned)time(NULL));
 		int n = atoi(argv[2]);
-		cout << "您希望使用哪种方式表示乘方？（m1：^/m2:**）" << endl;
+		cout << "您希望使用哪种方式表示乘方？(输入1选择模式1，输入2选择模式2）mode-1：^/mode-2:**" << endl;
 		int m;
 		cin >>m;
 		getchar();
@@ -32,7 +38,21 @@ int main(int argc, char * argv[])
 				ac++;
 		}
 		printf("本轮题目正确率：%d/%d\n", ac, n);
-		system("pause");
 	}
+	if (strcmp(argv[1], "-i")) //输入格式不合理的情况
+	{
+		printf("Please input in the correct form!\n");
+		system("pause");
+		return 0;
+	}
+	int flag = 0;
+	for (int i = 0; i <= strlen(argv[2]);i++)
+	{
+		if (argv[2][i]<'0' || argv[2][i]>'9')
+			flag = 1;
+	}
+	if (flag == 1) //输入非数字的情况
+		printf("Please input a NUMBER!\n");
+	system("pause");
 	return 0;
 }
